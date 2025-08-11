1369a93c2702e1e310db947923753519ce9a0988
ammiexx <animutalemneh632@gmail.com>
2025-08-11 15:47:33 +0300

    add changes

--- src/pages/RealHome.jsx
+++ src/pages/RealHome.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './realhome.css';
+import './RealHome.css';
 
 const RealHome = () => {
   const [products, setProducts] = useState([]);
