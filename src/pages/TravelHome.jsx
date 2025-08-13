8977b653cbcbf6d625a23364ffd3ffb9bbe533dd
ammiexx <animutalemneh632@gmail.com>
2025-08-13 13:45:49 +0300

    add changes

--- src/pages/TravelHome.jsx
+++ src/pages/TravelHome.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Travelhome.css';
+import './TravelHome.css';
 
 const TravelHome = () => {
   const [products, setProducts] = useState([]);
