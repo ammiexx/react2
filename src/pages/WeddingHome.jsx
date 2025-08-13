048ae3c92a500c3a1b07bee8293782e035f2f623
ammiexx <animutalemneh632@gmail.com>
2025-08-13 13:51:09 +0300

    add changes

--- src/pages/WeddingHome.jsx
+++ src/pages/WeddingHome.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Weddinghome.css';
+import './WeddingHome.css';
 
 const WeddingHome = () => {
   const [products, setProducts] = useState([]);
