99af985a1a3488af35ec0ad70be6535aa5adff77
ammiexx <animutalemneh632@gmail.com>
2025-08-23 14:58:32 +0300

    add changes

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,28 +1,9 @@
-
-import React from 'react';
-const Hamburger = ({ isOpen, toggle }) => {
+const Hamburger = ({ toggle }) => {
   return (
-    <button
-      onClick={toggle}
-      aria-label="Toggle menu"
-      type="button"
-      className="flex flex-col justify-between w-8 h-6 z-[1100] transition-transform duration-200 hover:scale-110 border border-transparent hover:border-white"
-    >
-      <span
-        className={`h-1 w-full bg-white rounded transition-transform duration-300 ease-in-out ${
-          isOpen ? 'rotate-45 translate-y-2' : ''
-        }`}
-      />
-      <span
-        className={`h-1 w-full bg-white rounded transition-opacity duration-300 ${
-          isOpen ? 'opacity-0' : 'opacity-100'
-        }`}
-      />
-      <span
-        className={`h-1 w-full bg-white rounded transition-transform duration-300 ease-in-out ${
-          isOpen ? '-rotate-45 -translate-y-2' : ''
-        }`}
-      />
+    <button onClick={toggle} className="relative w-6 h-6 flex flex-col justify-between items-center">
+      <span className="block h-0.5 w-full bg-white" />
+      <span className="block h-0.5 w-full bg-white" />
+      <span className="block h-0.5 w-full bg-white" />
     </button>
   );
 };
