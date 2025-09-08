5afa15a803b8d00e46c56f4f077ed46688bf8bb2
ammiexx <animutalemneh632@gmail.com>
2025-09-08 11:04:40 +0300

    add changes

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -474,7 +474,7 @@ if (!formData.email) {
         }
         setAuthWarning(false);
         setTimeout(() => {
-          navigate('/signup');  // ✅ React Router navigation
+          navigate('/login');  // ✅ React Router navigation
         }, 300);
       }}
       className="text-blue-600 underline hover:text-blue-800 transition duration-200"
