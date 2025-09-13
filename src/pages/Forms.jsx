f05cd96280914986aa88a46264d94c07e3944253
ammiexx <animutalemneh632@gmail.com>
2025-09-13 11:49:43 +0300

    add changes

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -213,7 +213,8 @@ if (!user) {
 };
 
   return (
-    <div className="max-w-3xl mx-auto p-8 bg-gray-100 shadow-xl rounded-xl mt-10 border border-gray-200">
+    <div className="max-w-3xl mx-auto p-8 bg-gray-100 shadow-xl rounded-xl mt-10 border border-gray-200 relative">
+
 
       <div className="text-center mb-8">
   <h1 className="text-3xl font-bold text-blue-800">🚀 Promote Your Product</h1>
