88caa9c08e33dcc377693365dce7285ae1efce00
ammiexx <animutalemneh632@gmail.com>
2025-10-06 16:06:00 +0300

    add changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './pages/ThemeContext.jsx'
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
- // ✅ Access your env variable
 console.log("Clerk Publishable Key:", clerkPubKey);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
