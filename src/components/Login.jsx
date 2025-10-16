f640a083e3e3bd12c9f25cf7aa805d9b24b5765f
ammiexx <animutalemneh632@gmail.com>
2025-10-16 18:21:53 +0300

    add changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
       <SignIn
         path="/login"
         routing="path"
-        signUpUrl="/signup"
+     
         afterSignUpUrl="/welcome"
         appearance={{
           layout: {
