85a1087bf64f3aad4f530d87facfeecd5610d44b
ammiexx <animutalemneh632@gmail.com>
2025-10-17 11:59:25 +0300

    add changes

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -33,6 +33,9 @@ const Form = () => {
   { value: '10', label: '10%' },
   { value: '15', label: '15%' },
   { value: '20', label: '20%' },
+  { value: '30', label: '30%' },
+  { value: '50', label: '40%' },
+  { value: '50', label: '50%' },
   { value: 'waiting', label: 'Waiting' },
 
 ];
