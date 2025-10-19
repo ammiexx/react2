054b6c9b04b7b6ad199d1f3a0bd7166da2d75e22
ammiexx <animutalemneh632@gmail.com>
2025-10-19 08:50:01 +0300

    add changes

--- src/components/Searching.jsx
+++ src/components/Searching.jsx
@@ -25,24 +25,24 @@ const Searching = ({ products, onFilter, onSubmit }) => {
     }
   };
   return (
-    <div className="relative w-72 sm:w-90 flex items-center">
+   <div className="relative w-full max-w-[400px] sm:max-w-[500px] md:max-w-[600px] flex items-center">
+  <input
+    type="text"
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)}
+    onKeyDown={handleKeyDown}
+    placeholder="Search Item, Location, Building, Co."
+    className="flex-1 h-11 text-sm text-gray-900 placeholder-gray-500 bg-white rounded-full pl-4 pr-12 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-400 shadow-md transition-all"
+  />
+  <button
+    type="button"
+    onClick={handleSearch}
+    className="absolute right-1 top-1/2 transform -translate-y-1/2 bg-yellow-400 hover:bg-yellow-500 h-8 w-8 sm:h-10 sm:w-10 flex items-center justify-center rounded-r-full text-white shadow-md transition-colors"
+  >
+    <MagnifyingGlassIcon className="h-5 w-5 sm:h-6 sm:w-6" />
+  </button>
+</div>
 
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={handleKeyDown}
-        placeholder="Search Item ,Location, Building,Co."
-       className="flex-1 h-11 sm:h-11 text-sm text-gray-900 placeholder-gray-500 bg-white rounded-full pl-4 pr-12 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-400 shadow-md transition-all"
-      />
-      <button
-        type="button"
-        onClick={handleSearch}
-        className="absolute right-1 top-1/2 transform -translate-y-1/2 bg-yellow-400 hover:bg-yellow-500    h-8 w-8 sm:h-10 sm:w-10 flex items-center justify-center rounded-r-full text-white shadow-md transition-colors"
-      >
-        <MagnifyingGlassIcon className="h-5 w-5 sm:h-6 sm:w-6" />
-      </button>
-    </div>
   );
 };
 export default Searching;
