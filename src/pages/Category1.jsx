f9583fd2a1e0de2e6529dd1f070c32d01488fa42
ammiexx <animutalemneh632@gmail.com>
2025-10-21 15:07:38 +0300

    add changes

--- src/pages/Category1.jsx
+++ src/pages/Category1.jsx
@@ -20,6 +20,8 @@ import security from "../assets/images/security.png";
 import telecom from "../assets/images/telecom.png";
 
 const categories = [
+   { name: "Men Fashions", image_url: men, path: "/men_shoes" },
+  { name: "Women Fashions", image_url: women, path: "/women_fashions" },
   { name: "Homes", image_url: home, path: "/homes" },
    { name: "Car Brands", image_url: car, path: "/car-brands" },
    { name: 'Travel services', image_url: travel, path: '/travels' },
@@ -27,12 +29,10 @@ const categories = [
   { name: 'Telecom Services', image_url: telecom, path: '/Telecome_Services' },
   { name: "Video & Photography essentials", image_url: photo, path: "/Video_And_Photography" },
   { name: " Dental Clinicis & Health and Beauty", image_url: beauty, path: "/health-and-beauties" },
-  { name: "Men Fashions", image_url: men, path: "/men_shoes" },
   { name: "Home Appliances", image_url: appliance, path: "/home-appliances" },
   { name: "Furniture & Interior Design materials", image_url: furniture, path: "/Furniture_And_Interior_design" },
   { name: "Watches & Jewelries", image_url: jewelries, path: "/jewelries" },
   { name: "Baby & Kids essentials", image_url: kids, path: "/Babies_And_Kids_Products" },
-  { name: "Women Fashions", image_url: women, path: "/women_fashions" },
   { name: "Fitness essentials", image_url: fitness, path: "/fitness_And_Sports_servies" },
   { name: "Farm products", image_url: farm, path: "/Agriculture_And_Livestock" },
   { name: "Art & Handicrafts", image_url: art, path: "/Art_And_Handicrafts" },
