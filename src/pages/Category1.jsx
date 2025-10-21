592aa647d75a3a4f14449b1cae0462684f39f5b7
ammiexx <animutalemneh632@gmail.com>
2025-10-21 12:40:07 +0300

    add changes

--- src/pages/Category1.jsx
+++ src/pages/Category1.jsx
@@ -29,14 +29,14 @@ const categories = [
   { name: "Men Fashions", image_url: men, path: "/men_shoes" },
   { name: "Home Appliances", image_url: appliance, path: "/home-appliances" },
   { name: "Furniture & Interior Design materials", image_url: furniture, path: "/Furniture_And_Interior_design" },
-  // { name: "Watches & Jewelries", image_url: jewelries, path: "/jewelries" },
-  // { name: "Baby & Kids essentials", image_url: kids, path: "/Babies_And_Kids_Products" },
-  // { name: "Women Fashions", image_url: women, path: "/women_fashions" },
-  // { name: "Fitness essentials", image_url: fitness, path: "/fitness_And_Sports_servies" },
-  // { name: "Farm products", image_url: farm, path: "/Agriculture_And_Livestock" },
-  // { name: "Art & Handicrafts", image_url: art, path: "/Art_And_Handicrafts" },
-  // { name: "Video & Photography essentials", image_url: photo, path: "/Video_And_Photography" },
-  // { name: "Printing & Publishing essentials", image_url: print, path: "/Printing_And_Publishing" },
+  { name: "Watches & Jewelries", image_url: jewelries, path: "/jewelries" },
+  { name: "Baby & Kids essentials", image_url: kids, path: "/Babies_And_Kids_Products" },
+  { name: "Women Fashions", image_url: women, path: "/women_fashions" },
+  { name: "Fitness essentials", image_url: fitness, path: "/fitness_And_Sports_servies" },
+  { name: "Farm products", image_url: farm, path: "/Agriculture_And_Livestock" },
+  { name: "Art & Handicrafts", image_url: art, path: "/Art_And_Handicrafts" },
+  { name: "Video & Photography essentials", image_url: photo, path: "/Video_And_Photography" },
+  { name: "Printing & Publishing essentials", image_url: print, path: "/Printing_And_Publishing" },
 ];
 
 const Category1 = () => {
